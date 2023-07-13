03cd3be56a7a07ceb0ea6deef858484f0116ca79
Shahid <shahidthoduvil.07@gmail.com>
2023-07-14 00:04:49 +0530

    Adding the Account models

---
+++ Account/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Occupation)
+admin.site.register(Category)
+admin.site.register(Address)
---
+++ Account/models.py
@@ -0,0 +1,120 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+
+ 
+# Create your models here.
+
+class AccountManger(BaseUserManager):
+    def _create_user(self,email,password,**extra_fields):
+       if not email:
+            raise ValueError('Your have not provided a valid e-mail address')
+       
+       email=self.normalize_email(email)
+       user=self.model(email=email,**extra_fields)
+       user.set_password(password)
+       user.save(using=self._db)
+       return user
+    
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+        
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+    
+
+
+
+class Category(models.Model):
+    Category_name=models.CharField(max_length=250,unique=True)
+    class Meta:
+        verbose_name = 'OCcupation'
+
+ 
+    def __str__(self):
+        return self.Category_name
+
+        
+class Occupation(models.Model):
+    Cat=models.ForeignKey(Category,on_delete=models.CASCADE)
+    titile=models.CharField(max_length=250,unique=True)
+
+    class Meta:
+        verbose_name = 'OCcupation'
+
+ 
+    def __str__(self):
+        return self.titile
+
+class User(AbstractBaseUser):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    username=models.CharField(max_length=50,unique=True)
+    email=models.EmailField(max_length=100,unique=True)
+    phone_number=models.CharField(max_length=50)
+    pic=models.ImageField(upload_to="pro_pic/",null=True,blank=True,default="static/img/userprofile.jpg" )
+    Occup=models.ForeignKey(Occupation,on_delete=models.CASCADE, blank=True , null=True)
+
+
+
+    #required
+
+
+    date_joined=models.DateTimeField(auto_now_add=True)
+    last_login=models.DateTimeField(auto_now_add=True)
+    is_admin=models.BooleanField(default=False)
+    is_staff=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+    is_available=models.BooleanField(default=False)
+
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username','first_name','last_name','phone_number']
+
+
+    objects=AccountManger()
+
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self,pars,obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self,add_label):
+        return True
+    
+
+
+
+class Address(models.Model):
+        user=models.ForeignKey(User,on_delete=models.CASCADE)
+        house_name=models.CharField(max_length=250,unique=False)
+        landmark=models.CharField(max_length=100)
+        pincode=models.IntegerField()
+        city=models.CharField(max_length=200,unique=False)
+        district=models.CharField(max_length=100,unique=False)
+        state=models.CharField(max_length=200,unique=False)
+        country=models.CharField(max_length=150,unique=False)
+        phone_number=models.CharField(max_length=100)
+        default=models.BooleanField(default=False)
+
+
+
+
+
+        
+
+            

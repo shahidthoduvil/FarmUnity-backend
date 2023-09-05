99b8d7231826eb692af44f19bf95717cf7440046
Shahid <shahidthoduvil.07@gmail.com>
2023-09-05 16:32:55 +0530

    updating asgi.py

--- Backend/asgi.py
+++ Backend/asgi.py
@@ -10,12 +10,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 import django
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 from channels.routing import ProtocolTypeRouter, URLRouter
 from Message.routing import websocket_urlpatterns
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
-django.setup()
+
 
 django_asgi_app = get_asgi_application()
 
